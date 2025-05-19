4f390a81c48cba023441d289d96775d51ab8e45c
RSAdrian9 <adrian4rs@gmail.com>
2025-05-20 00:26:32 +0200

    Update: minor changes

--- routes/api.php
+++ routes/api.php
@@ -14,18 +14,18 @@ require __DIR__.'/api/users.php';
 require __DIR__.'/api/companies.php';
 require __DIR__.'/api/schools.php';
 require __DIR__.'/api/internshipassignment.php';
+require __DIR__.'/api/auth.php';
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-// // Rutas públicas (Registro y Login)
+// Rutas públicas (Registro y Login)
 // Route::post('/register', [AuthController::class, 'register']);
 // Route::post('/login', [AuthController::class, 'login']);
 
-// // Rutas protegidas con autenticación
+// Rutas protegidas con autenticación
 // Route::middleware('auth:sanctum')->group(function () {
 //     Route::post('/logout', [AuthController::class, 'logout']);
 //     Route::resource('schools', SchoolApiController::class);
-//     Route::resource('students', StudentApiController::class);
 // });
